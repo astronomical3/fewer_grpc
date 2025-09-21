589befbcb4794477d39277e5cb4e812d88c7eaf9
Victoria Brunner <astrobrunner@gmail.com>
2025-09-21 13:13:52 -0400

    Added main server app setup
    All the core components of the Fewer Service Server Application were successfully defined, so I wrote out a simple script that uses flags of the 'go run [fewer_grpc/server/]app.go' command to set up either a production-level or development/test-level Fewer Service Server Application, so now programs that have the service's Client Application can now actually perform the core GetAggregatesStream RPC operation and hopefully receive a few responses for the many more requests they will send to the server.

---
+++ server/app.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/astronomical3/fewer_grpc/server/internal"
+)
+
+// Provide a name of the production-level or development-level server activity log file.
+//   The filename is relative to the fewer_grpc/server/serverlogs/ directory path.
+const serverLogFilename = "server.log"
+
+// Definition of the --address flag of the 'go run [fewer_grpc/server/]app.go' command.
+var address = flag.String("address", "localhost", "address of server to serve on")
+
+// Definition of the --port flag of the 'go run [fewer_grpc/server/]app.go' command.
+var port = flag.Int("port", 50051, "port of server to serve on")
+
+// Definition of the --prod flag of the 'go run [fewer_grpc/server/]app.go' command.
+var prod = flag.Bool("prod", true, "indicates whether server is production server or development server")
+
+func main() {
+	// Create a listener, lis, that will listen to requests to the address and port
+	//   provided by the 'go run' command flags.
+	addrString := fmt.Sprintf("%s:%d", *address, *port)
+	lis, err := net.Listen("tcp", addrString)
+	if err != nil {
+		log.Fatalf("fewer_grpc/server/app.go: net.Listen failed to listen: %v", err)
+	}
+
+	// Create a new GeneralFewerServer object.
+	genServer := internal.NewGeneralFewerServer(serverLogFilename, lis, *prod)
+
+	// Have the GeneralFewerServer object serve clients.  This method also handles
+	//   shutdowns or server failures.
+	genServer.ListenAndServe()
+}
